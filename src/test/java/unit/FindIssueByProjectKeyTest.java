3b16ee6c1f9480536a7a445a3c93b526fb6e1984
WhiteSnakeE <surtx0119@gmail.com>
2023-04-17 15:15:03 +0300

    added FindIssueByProjectKeyTest

--- src/test/java/unit/FindIssueByProjectKeyTest.java
+++ src/test/java/unit/FindIssueByProjectKeyTest.java
@@ -1,3 +1,35 @@
 package unit;
 
-public class FindIssueByProjectKeyTest {}
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.example.ProcessEnv;
+import org.example.task.FindIssueByProjectKeyTask;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class FindIssueByProjectKeyTest {
+    @Mock
+    private DelegateExecution execution;
+
+
+    @InjectMocks
+    private FindIssueByProjectKeyTask task;
+
+    @Test
+    public void givenIssueKey_whenStartMessageEvent_thenCheckOutput () {
+
+        when(execution.getVariable("IssueKey")).thenReturn("FIXBIT-18");
+
+        task.execute(execution);
+
+        verify(execution).setVariable(ProcessEnv.ISSUE_KEY, "FIXBIT-18");
+
+    }
+}
